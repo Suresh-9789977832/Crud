d3f09e79caaf56ccf9cc3830d8a5df34d764b768
Suresh@9789977832 <sanddysuresh@gmail.com>
2023-11-27 00:30:11 +0530

    port change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ dotenv.config()
 const express = require("express")
 const router=require ('./router/router')
 const app = express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8000
 
 app.use(express.json())
 
